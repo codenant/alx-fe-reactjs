f7d597af9b0b4ac1112c94bcddffdb13c6759dbe
codenant <e.g.dallo@gmail.com>
2024-08-14 22:15:38 +0400

    Adding routing using (BrowserRouter and Routes) to each of the webpages

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import NavBar from "./components/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
