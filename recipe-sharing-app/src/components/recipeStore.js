8e8d3c04892206f8b71ec4381b127fcb89d95376
codenant <e.g.dallo@gmail.com>
2024-08-21 23:50:44 +0400

    Added deletion and updating functionality to the store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,9 +1,19 @@
 import { create } from "zustand";
 
 const useRecipeStore = create((set) => ({
-  recipes: [],
+  recipes: [{ title: "lorem ipsum", description: "lorem ipsum" }],
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  deleteRecipes: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
   setRecipes: (recipes) => set({ recipes }),
 }));
 
