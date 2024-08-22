293ae0563ea5a85cded3f32f39f69f50c5ad4f13
codenant <e.g.dallo@gmail.com>
2024-08-22 17:28:59 +0400

    Integrated the search and filter stores to filter recipes according to the entered search terms

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,20 +1,43 @@
 import useRecipeStore from "./recipeStore";
 import { Link } from "react-router-dom";
+import SearchBar from "./SearchBar";
+import { useEffect } from "react";
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
+  const { searchTerm, filterRecipes, filteredRecipes } = useRecipeStore();
 
-  return (
-    <div>
-      {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-          <Link to={`/details/${recipe.id}`}>More Details</Link>
-        </div>
-      ))}
-    </div>
-  );
+  useEffect(() => {
+    filterRecipes();
+  }, [searchTerm, filterRecipes]);
+
+  if (searchTerm === "") {
+    return (
+      <div>
+        <SearchBar />
+        {recipes.map((recipe) => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+            <Link to={`/details/${recipe.id}`}>More Details</Link>
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <SearchBar />
+        {filteredRecipes.map((recipe) => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+            <Link to={`/details/${recipe.id}`}>More Details</Link>
+          </div>
+        ))}
+      </div>
+    );
+  }
 };
 
 export default RecipeList;
