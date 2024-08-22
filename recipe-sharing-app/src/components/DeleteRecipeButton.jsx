6af34122643c4f21039f17d9ec8e577e3ada1693
codenant <e.g.dallo@gmail.com>
2024-08-22 14:22:53 +0400

    Added the useNavigate function from react router to redirect to homepage after deletion of recipe + unary operator.

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,10 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import useRecipeStore from "./recipeStore";
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteButton = useRecipeStore((state) => state.deleteRecipes);
+  const navigate = useNavigate();
 
   const handleClick = () => {
-    deleteButton(recipeId);
+    deleteButton(+recipeId);
+    navigate("/");
   };
 
   return <button onClick={handleClick}>Delete</button>;
