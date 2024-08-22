b3d6302f02401a87a5b19a2d21b7a0468630e0a7
codenant <e.g.dallo@gmail.com>
2024-08-22 19:52:14 +0400

    Integrated the favorite list component

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import AddRecipeForm from "./components/AddRecipeForm";
+import FavoritesList from "./components/FavoritesList";
 import RecipeDetails from "./components/RecipeDetails";
 import RecipeList from "./components/RecipeList";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -14,6 +15,7 @@ function App() {
               <>
                 <RecipeList />
                 <AddRecipeForm />
+                <FavoritesList />
               </>
             }
           />
