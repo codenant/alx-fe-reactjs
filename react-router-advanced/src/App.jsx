7c54540a45f51deed95d0e083e0ec7a965415ac4
codenant <e.g.dallo@gmail.com>
2024-08-30 01:42:36 +0400

    Implemented routing

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Profile from "./components/Profile";
+import ProfileSettings from "./components/ProfileSettings";
+import ProfileDetails from "./components/ProfileDetails";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Profile />}>
+            <Route path=":userId" element={<Profile />} />
+            <Route path="settings" element={<ProfileSettings />} />
+            <Route path="details" element={<ProfileDetails />} />
+          </Route>
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
