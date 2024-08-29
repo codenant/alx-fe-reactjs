0a7e60b0eeb8058cebfc3ef54779ef0897488dbe
codenant <e.g.dallo@gmail.com>
2024-08-29 23:19:46 +0400

    Use react query useQuery hook to manage the fetching process, handle loading states, error handling, and data caching.

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,33 @@
+import { useQuery } from "react-query";
+
+const fetchPosts = async () => {
+  const response = await fetch(" https://jsonplaceholder.typicode.com/posts");
+  return response.json();
+};
+
+const PostsComponent = () => {
+  const { isLoading, isError, data, error } = useQuery(["posts"], fetchPosts, {
+    staleTime: 60000, //one minute until query transition from fresh to stale.
+    cacheTime: 300000, // five minutes until inactive queries will be removed from the cache.
+    refetchOnWindowFocus: true,
+    keepPreviousData: true,
+  });
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  return (
+    <div>
+      {data.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default PostsComponent;
