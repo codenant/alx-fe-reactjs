69e820eb1edfa60de64e8e169d1a4b7d89bb498d
codenant <e.g.dallo@gmail.com>
2024-08-29 17:40:40 +0400

    Integrated new component

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/formikForm";
 
 function App() {
   return (
     <>
+      <h2>Registration Form</h2>
       <RegistrationForm />
+      <h2>Formik Form</h2>
+      <FormikForm />
     </>
   );
 }
