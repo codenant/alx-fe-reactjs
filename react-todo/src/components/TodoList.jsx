a658898b3256da3a6befb04e889f78a462f7300d
codenant <e.g.dallo@gmail.com>
2024-08-30 18:50:30 +0400

    Simple todo list

---
+++ react-todo/src/components/TodoList.jsx
@@ -0,0 +1,44 @@
+function TodoList({ todos, setTodos }) {
+  const handleDelete = (id) => {
+    const filteredTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(filteredTodos);
+  };
+
+  const handleToggle = (id) => {
+    const completedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(completedTodos);
+    console.log(completedTodos);
+  };
+
+  return (
+    <>
+      <h2>Todo Items</h2>
+      {todos?.map((todo) => (
+        <div
+          key={todo.id}
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "8px",
+            marginBottom: "10px",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <input
+            type="checkbox"
+            name="todo"
+            id={todo.id}
+            onChange={() => handleToggle(todo.id)}
+          />
+          {todo.title}
+          <button onClick={() => handleDelete(todo.id)}>Delete</button>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default TodoList;
