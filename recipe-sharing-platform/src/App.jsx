de68ec0f1e9635f369f146d8e4eea61f67b94e16
codenant <e.g.dallo@gmail.com>
2024-09-09 20:01:31 +0400

    Integrated the homepage component

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,9 +1,10 @@
+import HomePage from "./components/HomePage";
 import "./index.css";
 
 function App() {
   return (
     <>
-      <h1 className="text-blue-500">Test</h1>
+      <HomePage />
     </>
   );
 }
