253eacf0fb41ec88ac8564f6de65076c8acf8477
codenant <e.g.dallo@gmail.com>
2024-09-09 20:25:16 +0400

    Modified grid layout for xs, sm, and md screens

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-3 place-items-center">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center">
       {recipes?.map((recipe) => (
         <div
           key={recipe.id}
