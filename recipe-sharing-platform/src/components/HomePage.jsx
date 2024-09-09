bf2dfd4878c0a68d273f4c028026faf9cd89de50
codenant <e.g.dallo@gmail.com>
2024-09-09 20:01:17 +0400

    Simple home page component that shows recipes

---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import data from "../data.json";
+
+const HomePage = () => {
+  const [recipes, setRecipes] = useState();
+
+  useEffect(() => {
+    setRecipes(data);
+  }, []);
+
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 place-items-center">
+      {recipes?.map((recipe) => (
+        <div
+          key={recipe.id}
+          className="bg-neutral-50 m-5 rounded w-8/12 shadow-md hover:shadow-lg transition ease-in-out duration-100"
+        >
+          <img
+            src={recipe.image}
+            className="m-auto py-5 hover:scale-105 transition ease-in-out duration-300"
+          />
+          <h1 className="font-bold text-yellow-600 mx-2">{recipe.title}</h1>
+          <p className="font-light text-zinc-600 p-2 text-base">
+            {recipe.summary}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default HomePage;
