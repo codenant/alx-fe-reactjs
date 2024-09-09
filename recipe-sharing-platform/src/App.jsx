8ea437815a88acface776528511674ff4c833a00
codenant <e.g.dallo@gmail.com>
2024-09-10 00:38:04 +0400

    Integrated the recipe adding form

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,12 +2,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import RecipeDetail from "./components/RecipeDetail";
 import "./index.css";
+import AddRecipeForm from "./components/AddRecipeForm";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <AddRecipeForm />
+              <HomePage />
+            </>
+          }
+        />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
       </Routes>
     </Router>
