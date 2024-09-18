a9b49c5a2425cced3fbc97290fbffea9c43ff389
codenant <e.g.dallo@gmail.com>
2024-09-18 20:39:35 +0400

    Integrated the search component

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,13 @@
-import "./App.css";
+import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Search from "./components/Search";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route />
+          <Route path="/" element={<Search />} />
         </Routes>
       </Router>
     </>
