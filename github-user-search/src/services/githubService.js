bba8ab9904b8d50f7e7c87719b7fac62a1644044
codenant <e.g.dallo@gmail.com>
2024-09-19 01:23:56 +0400

    Added advanced search functionality using querying

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,16 +1,31 @@
 import axios from "axios";
 
-function fetchUserData(username) {
-  return axios
-    .get(`https://api.github.com/users/${username}`)
-    .then((response) => {
-      console.log(response.data);
-      return response.data;
-    })
-    .catch((error) => {
-      console.error("Looks like we cant find the user");
-      throw error;
-    });
+function fetchUserData(username, location, repos) {
+  let query = "";
+
+  if (username) {
+    query += `user:${username}`;
+  }
+  if (location) {
+    query += `location:${location}`;
+  }
+  if (repos) {
+    query += `repos:>${repos}`;
+  }
+
+  return (
+    axios
+      // .get(`https://api.github.com/users/${username}`)
+      .get(`https://api.github.com/search/users?q=${query}`)
+      .then((response) => {
+        console.log(response.data);
+        return response.data.items;
+      })
+      .catch((error) => {
+        console.error("Looks like we cant find the user");
+        throw error;
+      })
+  );
 }
 
 export default fetchUserData;
